7ca824644be5f7dd1f589571709b466e56aea4f4
Sec4life.$ao <de7ket.3yonk.a@hotmail.com>
2023-03-29 14:30:35 +0300

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,15 @@
+// Server side C/C++ program to demonstrate Socket
+// programming
+
+#include<iostream>
+#include<fstream>
+#include<stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+using namespace std;
+
+
