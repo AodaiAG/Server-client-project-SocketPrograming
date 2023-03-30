f5abf923edd309eb06996de645a44a85615784c2
Sec4life.$ao <de7ket.3yonk.a@hotmail.com>
2023-03-30 12:07:06 +0300

    ss

--- Source.cpp
+++ Source.cpp
@@ -4,12 +4,30 @@
 #include<iostream>
 #include<fstream>
 #include<stdio.h>
-#include <unistd.h>
-#include <sys/socket.h>
+#include<winsock.h>
 #include <stdlib.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 
+class Socket
+{
+	struct sockaddr_in address;
+	
+	Socket()
+	{
+		int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+		int bind(int sockfd, const struct sockaddr* addr, socklen_t addrlen);;
+		int listen(int sockfd, LONG_MAX);
+		int new_socket = accept(int sockfd, struct sockaddr* addr, socklen_t * addrlen);
+	}
+};
+
+class clinet
+{
+	int G=connect(int sockfd, const struct sockaddr* addr, socklen_t addrlen);
+};
 using namespace std;
 
-
+void main()
+{
+	struct sockaddr_in address;
+	address.sin_addr = INADDR_ANY;
+}
